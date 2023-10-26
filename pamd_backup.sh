f8b2ed629f2303d449a62722cd3744959560b6e3
Prathap621 <53294410+Prathap621@users.noreply.github.com>
2023-10-26 16:43:56 +0530

    Update pamd_backup.sh

--- pamd_backup.sh
+++ pamd_backup.sh
@@ -4,10 +4,10 @@
 original_file="/etc/pam.d/common-password"
 backup_file="/etc/pam.d/common-password.bak"
 
-# Create a backup of the original file
+# backup file
 sudo cp "$original_file" "$backup_file"
 
-# Define the new content
+# update with new content
 new_content="password  sufficient   pam_unix.so use_authok md5 shadow
 password  sufficient   pam_krb5.so use_first_pass ignore_root
 password  required     pam_deny.so"
@@ -15,7 +15,7 @@ password  required     pam_deny.so"
 # Remove the original file's contents
 sudo truncate -s 0 "$original_file"
 
-# Add the new content to the original file
+# new content to the original file
 echo "$new_content" | sudo tee -a "$original_file"
 
 echo "Backup created at $backup_file"

3dcb652d4d02e3215452901d27f1032878ef930d
Prathap621 <53294410+Prathap621@users.noreply.github.com>
2023-10-26 16:25:29 +0530

    Update timeout.sh

--- timeout.sh
+++ timeout.sh
@@ -8,14 +8,19 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Define the desired TMOUT value
-TMOUT=$TMOUT_VALUE
-export TMOUT
+# Check if the TMOUT variable is already defined in /etc/profile
+if grep -q "TMOUT=" /etc/profile; then
+    echo "TMOUT is already defined in /etc/profile. Not making changes."
+else
+    # Define the desired TMOUT value
+    TMOUT=$TMOUT_VALUE
+    export TMOUT
 
-# Update /etc/profile with the new TMOUT setting
-echo "TMOUT=$TMOUT" | tee -a /etc/profile
+    # Update /etc/profile with the new TMOUT setting
+    echo "TMOUT=$TMOUT" | tee -a /etc/profile
 
-echo "TMOUT set to $TMOUT seconds in /etc/profile."
+    echo "TMOUT set to $TMOUT seconds in /etc/profile."
 
-# Apply the change immediately to the current shell session
-. /etc/profile
+    # Apply the change immediately to the current shell session
+    . /etc/profile
+fi

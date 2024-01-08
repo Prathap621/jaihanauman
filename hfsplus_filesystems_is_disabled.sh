0880c8980d251bc8a38735062a6c81268c0ccb78
Prathap621 <53294410+Prathap621@users.noreply.github.com>
2024-01-08 18:52:26 +0530

    Update hfsplus_filesystems_is_disabled.sh

--- hfsplus_filesystems_is_disabled.sh
+++ hfsplus_filesystems_is_disabled.sh
@@ -9,10 +9,10 @@ if [ ! -f "/etc/modprobe.d/hfsplus.conf" ]; then
     sudo chmod 644 /etc/modprobe.d/hfsplus.conf
 
     # Add the configuration line to disable cramfs in the hfsplus.conf file
-    echo "install cramfs /bin/true" | sudo tee /etc/modprobe.d/hfsplus.conf > /dev/null
+    echo "install hfsplus /bin/true" | sudo tee /etc/modprobe.d/hfsplus.conf > /dev/null
 
     # Inform the user that the mounting of cramfs filesystems has been disabled
-    echo "Mounting of cramfs filesystems has been disabled."
+    echo "Mounting of hfsplus filesystems has been disabled."
 else
     # Inform the user that the hfsplus.conf file already exists
     echo "hfsplus.conf file already exists. No action taken."
